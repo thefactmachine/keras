4fb1481e826d75bd0ef7c0c2673c7ab9d6936738
root <root@ip-172-31-37-5.ap-southeast-2.compute.internal>
2019-10-03 09:14:28 +0000

    added two files

---
+++ keras_setup.r
@@ -0,0 +1,16 @@
+install.packages("tensorflow")
+library("tensorflow")
+install_tensorflow(version = "gpu")
+
+install.packages("keras")
+library("keras")
+install_keras(tensorflow = "gpu")
+
+# saved at 19:06.
+
+# try this at 19:36
+
+# try this at 19:53
+
+# tried to save packages keras and tf.
+
